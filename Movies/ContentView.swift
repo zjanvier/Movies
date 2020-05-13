3aeb222808218b64df81104301fe4a3167d93e64
Janvier Zagabe <jzagabe@tfs.ca>
2020-05-13 18:14:09 -0400

    Initial Commit

---
+++ Movies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Janvier Rugomoka Zagabe on 2020-05-13.
+//  Copyright © 2020 Janvier Zagabe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
